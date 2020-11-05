481f7dfd6bb2ad258725452846957ada143f9a1e
Michael Murray <m.j.murray123@gmail.com>
2020-11-05 22:51:05 +0000

    Simplified datastore creation, added data store builder, and added support for iloc, loc

--- src/data_store/data_store.py
+++ src/data_store/data_store.py
@@ -1,8 +1,8 @@
-from src.data_store.data_type import DataType
-from typing import cast, get_args, get_type_hints, Optional, TypeVar
+from __future__ import annotations
+
+from typing import Any, Union, cast, Generic, get_type_hints, Type, TypeVar
 
 from pandas import DataFrame
-from pandas.core.arrays.sparse import dtype
 
 from src.data_store.column import Column, ColumnAlias
 
@@ -11,6 +11,8 @@ T = TypeVar("T", bound="DataStore")
 
 class DataStore:
     data_frame: DataFrame
+    loc: DataStore._LocIndexerFrame
+    iloc: DataStore._ILocIndexerFrame
 
     def __init_subclass__(cls) -> None:
         super(DataStore, cls).__init_subclass__()
@@ -18,14 +20,17 @@ class DataStore:
             col._name = name
             setattr(cls, name, col)
 
-    def __init__(self, column_data: Optional[dict[str, Column]] = None) -> None:
-        if column_data:
+    def __init__(self, **column_data: dict[str, Column]) -> None:
+        if len(column_data) > 0:
             column_data = {str(name): col for name, col in column_data.items()}
             self.data_frame = DataFrame(column_data)
             self._validate_data_frame()
             self._attach_columns()
         else:
             self.data_frame = DataFrame()
+
+        self.loc = DataStore._LocIndexerFrame(self)
+        self.iloc = DataStore._ILocIndexerFrame(self)
 
     @classmethod
     def columns(cls) -> dict[str, ColumnAlias]:
@@ -51,9 +56,13 @@ class DataStore:
         for name, col in columns.items():
             if name not in self.data_frame:
                 missing_cols.append(name)
-            data = Column(self.data_frame[name])
-            if data.dtype != col.dtype:
-                invalid_types.append(name)
+            else:
+                data = Column(self.data_frame[name])
+                if data.dtype != col.dtype:
+                    try:
+                        data = col(data)
+                    except:
+                        invalid_types.append(name)
 
         errors = []
         if len(missing_cols) != 0:
@@ -102,18 +111,61 @@ class DataStore:
     def __getitem__(self, item: str) -> Column:
         return self.columns()[item](self.data_frame[item])
 
-    def __str__(self: "DataStore") -> str:
+    def __str__(self) -> str:
         """TODO!"""
         if len(self.data_frame) == 0:
-            return (
-                f"Empty {self.__class__.__name__}"
-            )
+            return f"Empty {self.__class__.__name__}"
         else:
-            return (
-                f"{self.__class__.__name__}n{self.data_frame}"
-            )
+            return f"{self.__class__.__name__}n{self.data_frame}"
 
-    def __repr__(self: "DataStore") -> str:
+    def __repr__(self) -> str:
         """TODO!"""
         return str(self)
 
+    @classmethod
+    def builder(
+        cls: Type[T], **column_data: dict[str, Column]
+    ) -> DataStore._Builder[T]:
+        return DataStore._Builder[cls](cls, **column_data)
+
+    class _Builder(Generic[T]):
+        _store_class: Type[T]
+        _column_data: dict[str, Column]
+
+        def __init__(
+            self, store_class: Type[T], **column_data: dict[str, Column]
+        ) -> None:
+            self._store_class = store_class
+            self._column_data = column_data
+
+        def append(self, column_name: str, column_data) -> DataStore._Builder[T]:
+            self._column_data[str(column_name)] = column_data
+            return self
+
+        def __setitem__(self, column_name: str, column_data) -> None:
+            self._column_data[str(column_name)] = column_data
+
+        def build(self) -> T:
+            return self._store_class(**self._column_data)
+
+    class _ILocIndexerFrame(Generic[T]):
+        _data_store: T
+
+        def __init__(self, data_store: T) -> None:
+            self._data_store = data_store
+
+        def __getitem__(self, item: Union[int, slice]) -> T:
+            return self._data_store._new_data_copy(
+                self._data_store.data_frame.iloc[item]
+            )
+
+    class _LocIndexerFrame:
+        _data_store: T
+
+        def __init__(self, data_store: T) -> None:
+            self._data_store = data_store
+
+        def __getitem__(self, item: Union[Any, slice]) -> T:
+            return self._data_store._new_data_copy(
+                self._data_store.data_frame.loc[item]
+            )
--- test/data_store/test_data_store.py
+++ test/data_store/test_data_store.py
@@ -1,4 +1,4 @@
-from hamcrest import assert_that, equal_to
+from hamcrest import assert_that, equal_to, is_in
 from pytest import fail
 
 from src.data_store.column import Column
@@ -15,14 +15,10 @@ class TestDataStore:
     @classmethod
     def setup_class(cls) -> None:
         cls.test_store = ExampleStore(
-            {
-                ExampleStore.a: ["a", "b", "c"],
-                ExampleStore.b: [1, 2, 3],
-                ExampleStore.c: [True, False, True],
-            }
+            a=["a", "b", "c"], b=[1, 2, 3], c=[True, False, True]
         )
 
-    def test_get_column_by_name(self) -> None:
+    def test_get_column_by_name(self) -> None:       
         assert_that(self.test_store.a.tolist(), equal_to(["a", "b", "c"]))
         assert_that(self.test_store.b.tolist(), equal_to([1, 2, 3]))
         assert_that(self.test_store.c.tolist(), equal_to([True, False, True]))
@@ -33,22 +29,50 @@ class TestDataStore:
         assert_that(self.test_store["c"].tolist(), equal_to([True, False, True]))
 
     def test_missing_columns(self) -> None:
-        fail("Not Implemented")
+        try:
+            ExampleStore(a=["a", "b", "c"], b=[1, 2, 3])
+        except Exception as e:
+            assert_that("Column data was missing for: ['c']", is_in(str(e)))
 
     def test_castable_types(self) -> None:
-        fail("Not Implemented")
+        test_store = ExampleStore(a=["a", "b", "c"], b=[1, 2, 3], c=[1, 0, 1])
+        assert_that(test_store["a"].tolist(), equal_to(["a", "b", "c"]))
+        assert_that(test_store["b"].tolist(), equal_to([1, 2, 3]))
+        assert_that(test_store["c"].tolist(), equal_to([True, False, True]))
 
     def test_missing_types(self) -> None:
-        fail("Not Implemented")
+        try:
+
+            class MissingClass:
+                ...
+
+            class TempStore(DataStore):
+                a: Column[MissingClass]
+
+        except Exception as e:
+            assert_that(
+                "Failed to find column types for the following columns: ['a']",
+                is_in(str(e)),
+            )
 
     def test_invalid_types(self) -> None:
-        fail("Not Implemented")
+        try:
+            ExampleStore(a=["a", "b", "c"], b=[1, 2, 3], c=[1.23, 2.23, 3.23])
+        except Exception as e:
+            assert_that("Invalid types provided for: ['c']", is_in(str(e)))
 
     def test_builder(self) -> None:
-        fail("Not Implemented")
+        builder = ExampleStore.builder()
+        builder["a"] = ["a", "b", "c"]
+        builder.append("b", [1, 2, 3]).append(ExampleStore.c, [True, False, True])
+        test_store = builder.build()
+        assert_that(test_store.a.tolist(), equal_to(["a", "b", "c"]))
+        assert_that(test_store.b.tolist(), equal_to([1, 2, 3]))
+        assert_that(test_store.c.tolist(), equal_to([True, False, True]))
 
     def test_reset_index(self) -> None:
-        fail("Not Implemented")
+        test_slice = self.test_store.iloc[0:2]
+        pass
 
     def test_contains(self, key) -> None:
         fail("Not Implemented")
